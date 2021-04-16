af342defbc6ae78d88cd7afa792533d66b2f899a
Adi <adhiraj.sirohi@gmail.com>
2021-04-16 12:14:31 +0530

    v 1.0.0
    Initial Release

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+<h2 align="center">Changelog</h2>
+
+### Table of Contents
+
+- [v1.0.0](#v1.0.0)
+- [Versioning System](#versioning-system)
+- [License](#license)
+
+### v1.0.0 
+
+- Initial Commit 
+
+### Versioning System
+
+Lighthaus and all it's projects use [Semantic Versioning](https://semver.org/).
+
+### License
+
+_Copyright © 2020-Present Lighthaus Theme_<br>
+_Copyright © 2020-Present Adhiraj Sirohi_<br>
+_Copyright © 2020-Present Vasundhara Sharma_
+
+<p align="left"><a href="https://github.com/lighthaus-theme/fish/blob/main/LICENSE"><img src="https://img.shields.io/static/v1.svg??style=flat&logo=appveyore&label=License&message=MIT&colorA=1C918A&colorB=50C16E"/></a></p>
+
+<p align="center"><img src="https://raw.githubusercontent.com/lighthaus-theme/lighthaus/9e5cf66db03fc3e183e6cfbf7c4c04263a4f23df/ImageResources/lighthaus-border.svg"><p>
---
+++ conf.d/lighthaus.fish
@@ -0,0 +1,56 @@
+# Lighthaus Color theme for Fish Shell
+# GIT: https://github.com/lighthaus-theme/fish
+# Author: Adhiraj Sirohi (https://github.com/brutuski)
+#         Vasundhara Sharma (https://github.com/vasundhasauras)
+
+# Copyright © 2021-Present Lighthaus Theme
+# Copyright © 2021-Present Adhiraj Sirohi
+# Copyright © 2021-Present Vasundhara Sharma
+
+# LIGHTHAUS COLORS
+set -l lhBlack 18191E
+set -l lhBlack2 8E8D8D
+set -l lhRed FC2929
+set -l lhRed2 FF5050
+set -l lhGreen 44B273
+set -l lhGreen2 50C16E
+set -l lhOrange E25600
+set -l lhOrange2 ED722E
+set -l lhBlue 1D918B
+set -l lhBlue2 47A8A1
+set -l lhPurple D16BB7
+set -l lhPurple2 D68EB2
+set -l lhCyan 00BFA4
+set -l lhCyan2 5AD1AA
+set -l lhWhite CCCCCC
+set -l lhWhite2 FFFADE
+
+set -l lhFg FFEE79
+set -l lhSelFg FF4D00
+set -l lhSelBg 090B26
+set -l lhBold FFFF00
+
+set -g fish_color_normal $lhFg
+set -g fish_color_command $lhGreen2
+set -g fish_color_quote $lhCyan2
+set -g fish_color_redirection $lhFg
+set -g fish_color_end $lhOrange2
+set -g fish_color_error $lhRed
+set -g fish_color_param $lhOrange2
+set -g fish_color_comment $lhWhite
+set -g fish_color_match $lhOrange
+set -g fish_color_search_match $lhBold
+set -g fish_color_operator $lhCyan
+set -g fish_color_escape $lhBlue2
+set -g fish_color_cwd $lhCyan2
+set -g fish_color_autosuggestion $lhBlack2
+set -g fish_color_user $lhGreen2
+set -g fish_color_host $lhBlue2
+set -g fish_color_cancel $lhRed2
+
+set -g fish_pager_color_prefix $lhBlue2
+set -g fish_pager_color_completion $lhCyan2
+set -g fish_pager_color_description $lhWhite
+set -g fish_pager_color_progress $lhBlack2
+set -g fish_pager_color_secondary $lhBlack2
+
